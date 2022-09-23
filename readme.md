83c967316769969112a1bed02c39479d262145b0
Felix Tietjen <felix@tietjen.it>
2022-09-23 23:01:28 +0200

    refactor: initial cypress setup; adjust ts for cypress

--- readme.md
+++ readme.md
@@ -1,3 +1,7 @@
 # Fleet Management System
 
 Manage your cars - perfectly!
+
+## Tests
+
+Before opening the tests for the first time, run `yarn run cypress open` to set up cypress.
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import mkcert from "vite-plugin-mkcert";
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
+import mkcert from "vite-plugin-mkcert"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), mkcert()],
   server: {
     open: "/",
+    port: 3000,
   },
-});
+})
