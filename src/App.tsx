4ba46882480775f5ec607efd9aef4ae86ce297cc
Felix Tietjen <felix@tietjen.it>
2022-09-24 00:00:49 +0200

    feat: initialize mantine

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,14 @@
-const App = () => <>Hello World!</>
+import { MantineProvider, Container, Box } from "@mantine/core"
+
+const App = () => (
+  <MantineProvider
+    withGlobalStyles
+    withNormalizeCSS
+    theme={{ colorScheme: "dark" }}
+  >
+    <Box m={40} />
+    <Container>Hello World!</Container>
+  </MantineProvider>
+)
 
 export default App
